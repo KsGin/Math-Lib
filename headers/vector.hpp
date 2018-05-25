aeebe7487f672cd379d3491a59d76623d8400ea5
KsGin <imqqyangfan@gmail.com>
2018-05-25 13:36:54 +0800

    update : 2018.5.25 by ksgin : Update vector2 and test

--- headers/vector.hpp
+++ headers/vector.hpp
@@ -7,12 +7,116 @@
 #ifndef MATHLIB_VECTOR_HPP
 #define MATHLIB_VECTOR_HPP
 
+#include <cmath>
+
 namespace Math {
 
     /**
      *  Class Vector2
      */
     class Vector2 {
+
+    public:
+        /**
+         * x , y value
+         */
+        float x , y;
+
+        /**
+         * empty construct
+         */
+        Vector2(){}
+
+        /**
+         * value copy construct
+         * @param _x x value
+         * @param _y y value
+         */
+        Vector2(float _x , float _y){
+            x = _x;
+            y = _y;
+        }
+
+        /**
+         * object copy construct
+         * @param v2 vector object reference
+         */
+        Vector2(Vector2& v2){
+            x = v2.x;
+            y = v2.y;
+        }
+
+        /**
+         * add function
+         * @param v2 vector object reference
+         * @return result
+         */
+        Vector2& operator+(const Vector2& v2){
+            x += v2.x;
+            y += v2.y;
+            return *this;
+        }
+
+        /**
+         * sub function
+         * @param v2 vector object reference
+         * @return result
+         */
+        Vector2& operator-(const Vector2& v2){
+            x -= v2.x;
+            y -= v2.y;
+            return *this;
+        }
+
+        /**
+         * equal function
+         * @param v2 vector object reference
+         * @return result (ture or flase)
+         */
+        bool operator==(const Vector2& v2) const {
+            return x == v2.x && y == v2.y;
+        }
+
+        /**
+         * negate function
+         * @return reslut
+         */
+        Vector2& negate(){
+            x = -x;
+            y = -y;
+            return *this;
+        }
+
+        /**
+         * scale function
+         * @param scale scale value
+         * @return result
+         */
+        Vector2& scale(float scale){
+            x *= scale;
+            y *= scale;
+            return *this;
+        }
+
+        /**
+         * length function
+         * @return result
+         */
+        float length(){
+            return sqrt(x * x + y * y);
+        }
+
+        /**
+         * normalize function
+         * @return result
+         */
+        Vector2& normalize(){
+            auto length = this->length();
+            if (length == 0) return *this;
+            x /= length;
+            y /= length;
+            return *this;
+        }
 
     };
 
--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -6,3 +6,70 @@
 
 #include "../headers/vector.hpp"
 #include <gtest.h>
+
+using namespace Math;
+
+/**
+ * Vector2 construct() test function
+ */
+TEST(Vector2 , Construct){
+    Vector2 *v1 = new Vector2(1 , 1);
+    ASSERT_TRUE(v1 != nullptr);
+
+    Vector2 *v2 = new Vector2(*v1);
+    ASSERT_TRUE(v2 != nullptr);
+
+    delete v1;
+    delete v2;
+}
+
+/**
+ * Vector2 add() test function
+ */
+TEST(Vector2 , Add){
+    EXPECT_EQ(Vector2(1 , 1) + Vector2(2 , 2) , Vector2(3 , 3));
+    EXPECT_EQ(Vector2(1.5 , 1) + Vector2(2 , 2) , Vector2(3.5 , 3));
+    EXPECT_EQ(Vector2(1 , 1.5) + Vector2(2 , 2) , Vector2(3 , 3.5));
+    EXPECT_EQ(Vector2(1.5 , 1) + Vector2(2.5 , 2) , Vector2(4 , 3));
+}
+
+/**
+ * Vector2 sub() test function
+ */
+TEST(Vector2 , Sub){
+    EXPECT_EQ(Vector2(3 , 3) - Vector2(2 , 2) , Vector2(1 , 1));
+    EXPECT_EQ(Vector2(3.5 , 3) - Vector2(2 , 2) , Vector2(1.5 , 1));
+    EXPECT_EQ(Vector2(3 , 3.5) -Vector2(2 , 2) , Vector2(1 , 1.5));
+    EXPECT_EQ(Vector2(4 , 3) - Vector2(2.5 , 2) , Vector2(1.5 , 1));
+}
+
+/**
+ * Vector2 length() test function
+ */
+TEST(Vector2 , Length){
+    EXPECT_LE(fabs(Vector2(1 , 1).length() - sqrt(2)) , 0.001);
+    EXPECT_LE(fabs(Vector2(2 , 2).length() - sqrt(8)), 0.001);
+    EXPECT_LE(fabs(Vector2(5 , 6).length() - sqrt(pow(5 , 2) + pow(6 , 2))) , 0.001);
+    EXPECT_LE(fabs(Vector2(1.6 , 1.7).length() - sqrt(pow(1.6 , 2) + pow(1.7 , 2))) , 0.001);
+    EXPECT_LE(fabs(Vector2(12.9 , 1.1).length() - sqrt(pow(12.9 , 2) + pow(1.1 , 2))) , 0.001);
+}
+
+/**
+ * Vector2 negate() test function
+ */
+TEST(Vector2 , Negate){
+    EXPECT_EQ(Vector2(1 , 1).negate() , Vector2(-1  , -1));
+    EXPECT_EQ(Vector2(1 , -1).negate() , Vector2(-1  , 1));
+    EXPECT_EQ(Vector2(0 , 1).negate() , Vector2(0  , -1));
+}
+
+/**
+ * Vector2 normalize() test function
+ */
+TEST(Vector2 , Normalize){
+    EXPECT_LE(fabs(Vector2(1 , 1).normalize().length() - 1) , 0.001);
+    EXPECT_LE(fabs(Vector2(11.2 , 12.1).normalize().length() - 1) , 0.001);
+    EXPECT_LE(fabs(Vector2(13 , 21).normalize().length() - 1) , 0.001);
+    EXPECT_LE(fabs(Vector2(111.11 , 123.1).normalize().length() - 1) , 0.001);
+    EXPECT_LE(fabs(Vector2(1.1 , 109).normalize().length() - 1) , 0.001);
+}
