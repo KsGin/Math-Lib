2210c425ad4625219608e34fac1c4e25cfc9f240
KsGin <imqqyangfan@gmail.com>
2018-12-25 23:22:49 +0800

    Update Matrix::perspectiveFovLH Function

--- sources/matrix.hpp
+++ sources/matrix.hpp
@@ -306,7 +306,7 @@ namespace Math {
             return Matrix(
                     t / aspect, 0, 0, 0,
                     0, t, 0, 0,
-                    0, 0, -far / (near / far), 1,
+                    0, 0, -far / (near - far), 1,
                     0, 0, near * far / (near - far), 0
             );
         }
